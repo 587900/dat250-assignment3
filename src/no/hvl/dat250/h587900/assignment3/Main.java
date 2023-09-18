bb7af280e12a414add0ef7c63e6a0586b465401b
KjetilBerg <kjetil.a.berg@hotmail.com>
2023-09-18 11:19:29 +0200

    set package

--- src/no/hvl/dat250/h587900/assignment3/Main.java
+++ src/no/hvl/dat250/h587900/assignment3/Main.java
@@ -1,5 +1,12 @@
+package no.hvl.dat250.h587900.assignment3;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    private static void insertDocument() {
+
+    }
+
 }
